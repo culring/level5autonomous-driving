7a11e98a677d91bd5840194f42b51c792b158a2e
Grzegorz Kuduk <grzegorz07@gmail.com>
2018-01-04 12:09:50 +0100

    basic stuff (subject to change)

--- Model.cpp
+++ Model.cpp
@@ -1,1 +1,81 @@
 #include "Model.h"
+
+
+Model::Model()
+{
+	vehicle = new Car;
+	map = new Track;
+}
+
+Model::~Model()
+{
+	delete vehicle;
+	delete map;
+}
+
+
+
+Car::Car()
+{
+	x = 0;
+	y = 0;
+	dir = 0.0f;
+}
+
+Car::~Car()
+{
+	
+}
+
+
+Track::Track()
+{
+	k = 0;
+	segList = new Segment[TRACK_SIZE];
+}
+
+Track::~Track()
+{
+	delete[] segList;
+}
+	
+void Track::Update(float posX, float posY, float faceDir, int segmentType, float rads)
+{
+	segList[k].setSegment(float posX, float posY, float faceDir, int segmentType, float rads);
+	
+	k = ++k % TRACK_SIZE;
+}
+
+
+
+Segment::Segment()
+{
+	x = 0;
+	y = 0;
+	dir = 0;
+	segType = 0;
+	rad = 0.0f;
+}
+
+Segment::Segment(float posX, float posY, float faceDir, int segmentType, float rads)
+{
+	x = posX;
+	y = posY;
+	dir = faceDir;
+	segType = segmentType;
+	rad = rads;
+}
+
+~Segment::Segment()
+{
+	
+}
+
+void Segment::setSegment(float posX, float posY, float faceDir, int segmentType, float rads)
+{
+	x = posX;
+	y = posY;
+	dir = faceDir;
+	segType = segmentType;
+	rad = rads;
+}
--- Model.h
+++ Model.h
@@ -1,1 +1,50 @@
+#define TRACK_SIZE 16
 
+class Model
+{
+public:
+	Car *vehicle;
+	Track *map;
+	
+	Model();
+	~Model();
+};
+
+
+class Car
+{
+public:
+	float x, y, dir;			// car's current position and direction it's facing
+	
+	Car();
+	~Car();
+};
+
+
+class Track
+{
+public:
+	Segment *segList;			// current track
+	int k;
+	
+	Track();
+	~Track();
+	
+	void Update(float posX, float posY, float faceDir, int segmentType, float rads);				// replace old segments with new ones
+};
+
+
+class Segment
+{
+public:
+	float x, y, dir;
+	int segType;
+	
+	float rad;					// turn degree (0 for straight segments)
+	
+	Segment();
+	Segment(float posX, float posY, float faceDir, int segmentType, float rads);
+	~Segment();
+	
+	void setSegment(float posX, float posY, float faceDir, int segmentType, float rads);
+};
