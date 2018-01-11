75cdc4b29d04b6c597beaaa2ef98352d64d35600
Grzegorz Kuduk <grzegorz07@gmail.com>
2018-01-11 18:54:23 +0100

    updated model

--- Model.cpp
+++ Model.cpp
@@ -1,52 +1,72 @@
 #include "Model.h"
 
-
-Race::Race()
-{
-	vehicle = new Car;
-	map = new Track;
-}
-
-Race::~Race()
-{
-	delete vehicle;
-	delete map;
-}
-
-
+/* Car */
 
 Car::Car()
 {
-	x = 0;
-	y = 0;
+	x = 0.0f;
+	y = 0.0f;
 	dir = 0.0f;
+	
+	speed = 0.0f;
+	turn = 0.0f;
 }
 
-Car::~Car()
+Car::~Car() {}
+
+void Car::turnLeft()
 {
-
+	if (turn >= -60.0f)
+		turn = turn - 1.0f;
 }
 
-
-Track::Track()
+void Car::turnRight()
 {
-	k = 0;
-	segList = new Segment[TRACK_SIZE];
+	if (turn <= 60.0f)
+		turn = turn + 1.0f;
 }
 
-Track::~Track()
+void Car::stabilize()
 {
-	delete[] segList;
+	float rate = 2.0f;
+	
+	if (turn > 0.0f)
+	{
+		if (turn > rate)
+			turn = turn - rate;
+		else
+			turn = 0.0f;
+	}
+	else if (turn < 0.0f)
+	{
+		if (turn < -rate)
+			turn = turn + rate;
+		else
+			turn = 0.0f;
+	}
 }
-
-void Track::update(float posX, float posY, float faceDir, int segmentType, float rads)
+	
+void Car::accel()
 {
-	segList[k].setSegment(posX, posY, faceDir, segmentType, rads);
-
-	k = ++k % TRACK_SIZE;
+	float rate = 0.5f;
+	
+	if (speed < 50.0f - rate)
+		speed = speed + 0.5f;
+	else
+		speed = 50.0f;
 }
 
+void Car::slow()
+{
+	float rate = 2.0f;
+	
+	if (speed > rate)
+		speed = speed - 2.0f;
+	else
+		speed = 0.0f;
+}
 
+/* Segment */
 
 Segment::Segment()
 {
@@ -66,10 +86,7 @@ Segment::Segment(float posX, float posY, float faceDir, int segmentType, float r
 	rad = rads;
 }
 
-Segment::~Segment()
-{
-
-}
+~Segment::Segment() {}
 
 void Segment::setSegment(float posX, float posY, float faceDir, int segmentType, float rads)
 {
@@ -78,4 +95,70 @@ void Segment::setSegment(float posX, float posY, float faceDir, int segmentType,
 	dir = faceDir;
 	segType = segmentType;
 	rad = rads;
+}
+
+
+/* Track */
+
+Track::Track()
+{
+	k = 0;
+	segList = new Segment[TRACK_SIZE];
+}
+
+Track::~Track()
+{
+	delete[] segList;
+}
+
+void Track::update(float posX, float posY, float faceDir, int segmentType, float rads)
+{
+	segList[k].setSegment(float posX, float posY, float faceDir, int segmentType, float rads);
+
+	k = ++k % TRACK_SIZE;
+}
+
+
+/* Race */
+
+Race::Race()
+{
+	vehicle = new Car;
+	map = new Track;
+}
+
+Race::~Race()
+{
+	delete vehicle;
+	delete map;
+}
+
+Segment* Race::getMap()
+{
+	return map->segList;		// ?
+}
+
+float Race::getCarX()
+{
+	return vehicle.x;
+}
+
+float Race::getCarY()
+{
+	return vehicle.y;
+}
+
+float Race::getCarDir()
+{
+	return vehicle.dir;
+}
+
+float Race::getCarSpeed()
+{
+	return vehicle.speed;
+}
+
+float Race::getCarTurn()
+{
+	return vehicle.turn;
 }
--- Model.h
+++ Model.h
@@ -4,25 +4,36 @@ class Car
 {
 public:
 	float x, y, dir;			// car's current position and direction it's facing
+	float speed;
+	float turn;					// negative values for turn to the left, positive for turn to the right
 	
 	Car();
 	~Car();
+	
+	void turnLeft();
+	void turnRight();
+	void stabilize();
+	
+	void accel();
+	void slow();
 };
+
 
 class Segment
 {
 public:
 	float x, y, dir;
 	int segType;
-
+	
 	float rad;					// turn degree (0 for straight segments)
-
+	
 	Segment();
 	Segment(float posX, float posY, float faceDir, int segmentType, float rads);
 	~Segment();
 	
 	void setSegment(float posX, float posY, float faceDir, int segmentType, float rads);
 };
+
 
 class Track
 {
@@ -36,12 +47,21 @@ public:
 	void update(float posX, float posY, float faceDir, int segmentType, float rads);				// replace old segments with new ones
 };
 
+
 class Race
 {
 public:
 	Car *vehicle;
 	Track *map;
-
+	
 	Race();
 	~Race();
+	
+	Segment* getMap();
+	
+	float getCarX();
+	float getCarY();
+	float getCarDir();
+	float getCarSpeed();
+	float getCarTurn();
 };
