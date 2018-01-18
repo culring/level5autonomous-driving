ee35a6c442b0074cfd251e3766253050b80049c1
culring <ravw96@gmail.com>
2018-01-18 20:29:34 +0100

    Update ViewCar.h

--- ViewCar.h
+++ ViewCar.h
@@ -4,18 +4,29 @@
 
 #ifndef LEVEL5AUTONOMOUS_DRIVING_CAR_H
 #define LEVEL5AUTONOMOUS_DRIVING_CAR_H
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Sprite.hpp>
 
 class ViewCar {
+	// window
+	sf::RenderWindow *m_window;
     // current car's position
     float m_positionX, m_positionY;
     // current car's orientation
     float m_direction;
+	//sprite
+	sf::Sprite sprite;
+	sf::Texture texture;
 
     void move();
 
 public:
+	ViewCar(sf::RenderWindow *window);
+
     void updatePosition(float positionX, float positionY);
     void updateDirection(float direction);
+	void display();
 };
 
 #endif //LEVEL5AUTONOMOUS_DRIVING_CAR_H
